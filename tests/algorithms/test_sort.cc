6d0755e698b99d03d4357bcee3acb625362d8581
Matthew Truscott <matthew.truscott@turintech.ai>
2023-12-13 15:46:01 +0000

    test: unit tests for sort

--- tests/algorithms/test_sort.cc
+++ tests/algorithms/test_sort.cc
@@ -39,3 +39,31 @@ TEST_CASE("Sort::DutchFlagPartition::Test", "[test][algorithms][sort]") {
   Sort::DutchFlagPartition(v2, 4);
   REQUIRE(v2 == vref);
 }
+
+TEST_CASE("Sort::DutchFlagPartition::Benchmark", "[benchmark][algorithms][sort]") {
+  std::vector v0 = GenVector::RandomVector(100000, 10);
+  BENCHMARK("DutchFlagPartition(100000, 3)") { Sort::DutchFlagPartition(v0, 3); };
+  std::vector v1 = GenVector::RandomVector(100000, 100);
+  BENCHMARK("DutchFlagPartition(100000, 50)") { Sort::DutchFlagPartition(v1, 50); };
+}
+
+TEST_CASE("Sort::MaxN::Test", "[test][algorithms][sort]") {
+  std::vector<int> v0 = {1, 3, 2, 5, 4};
+  std::vector<int> v0_max3 = Sort::MaxN(v0, 3);
+  REQUIRE(v0_max3 == std::vector<int>({5, 4, 3}));
+  std::vector<int> v0_max2 = Sort::MaxN(v0, 2);
+  REQUIRE(v0_max2 == std::vector<int>({5, 4}));
+
+  std::vector<int> v1 = {1, 1, 2, 2, 3};
+  std::vector<int> v1_max3 = Sort::MaxN(v1, 3);
+  REQUIRE(v1_max3 == std::vector<int>({3, 2, 2}));
+  std::vector<int> v1_max2 = Sort::MaxN(v1, 2);
+  REQUIRE(v1_max2 == std::vector<int>({3, 2}));
+}
+
+TEST_CASE("Sort::MaxN::Benchmark", "[benchmark][algorithms][sort]") {
+  std::vector v0 = GenVector::RandomVector(100000, 10);
+  BENCHMARK("MaxN(100000, 10)") { return Sort::MaxN(v0, 10); };
+  std::vector v1 = GenVector::RandomVector(100000, 100);
+  BENCHMARK("MaxN(100000, 100)") { return Sort::MaxN(v1, 100); };
+}
