8b8805e80df647ba86c071d44030d1f56af3709c
Matthew Truscott <matthew.truscott@turintech.ai>
2023-12-13 15:26:51 +0000

    test: dutch flag tests

--- tests/algorithms/test_sort.cc
+++ tests/algorithms/test_sort.cc
@@ -24,3 +24,18 @@ TEST_CASE("Sort::SortVector::Benchmark", "[benchmark][algorithms][sort]") {
   std::vector v1 = GenVector::RandomVector(100000, 100);
   BENCHMARK("SortVector(100000, 100)") { Sort::SortVector(v1); };
 }
+
+TEST_CASE("Sort::DutchFlagPartition::Test", "[test][algorithms][sort]") {
+  std::vector<int> v0 = {1, 3, 2, 5, 4};
+  Sort::DutchFlagPartition(v0, 3);
+  REQUIRE(v0.at(2) == 3);
+
+  std::vector<int> v1 = {5, 4, 3, 2, 1};
+  Sort::DutchFlagPartition(v1, 2);
+  REQUIRE(v1.at(1) == 2);
+
+  std::vector<int> v2 = {2, 2, 1, 1, 3};
+  std::vector<int> vref = v2;
+  Sort::DutchFlagPartition(v2, 4);
+  REQUIRE(v2 == vref);
+}
